42ad5919b9a7a5835324b682644904f0fc75c5af
za-king <muhammadzakysyukur@gmail.com>
2022-06-06 19:47:37 +0700

    conflic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 import routes from './routes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-=======
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> ee85afbae664fbdfa7cfc995539cf90f2dcf9223
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           {
@@ -22,9 +18,6 @@ function App() {
           }
         </Routes>
       </BrowserRouter>
-=======
-      <h2>HELLO WORLD</h2>
->>>>>>> ee85afbae664fbdfa7cfc995539cf90f2dcf9223
     </div>
   );
 }
