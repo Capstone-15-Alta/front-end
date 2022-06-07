690d1fc661dc95a2638ad5a9f4557d53edcf79d6
muhamadjaya <ahmadzainsiji@gmail.com>
2022-06-07 16:12:03 +0700

    make navigation bar and buat thread page

--- src/components/Navigationbar/Navigationbar.jsx
+++ src/components/Navigationbar/Navigationbar.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "./Navigationbar.scss";
+
+import { Link, NavLink } from "react-router-dom";
+// import NavLogo from "../../../public/assets/logo/Group2.png";
 
 const Navigationbar = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top customNav shadow-sm">
+        <div className="container-fluid">
+          <Link className="navbar-brand navBrand" to="/">
+            <img src="/assets/logo/Group2.png" alt="logo" className="navLogo" />
+            <span className="textLogo">Forum Group Diskusi</span>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            {/* <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Features
+                </Link>
+              </li>
+              
+            </ul> */}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigationbar;
---
+++ src/pages/BuatThread/BuatThread.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BuatThread.scss";
+
+import Navigationbar from "../../components/Navigationbar/Navigationbar";
+
+const BuatThread = () => {
+  return (
+    <div>
+      <Navigationbar />
+      {/* <h1>Buat Thread</h1> */}
+    </div>
+  );
+};
+
+export default BuatThread;
