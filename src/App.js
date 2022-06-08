8f438b9d4ebc37c138ecfed5fccf1d3e132a146c
DargonFly378 <hafichever@gmail.com>
2022-06-08 14:14:14 +0700

    "update"

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import routes from './routes';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routes from "./routes";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Signin from "./pages/Signin/Signin";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup/Signup";
-import Home from "./pages/Home/Home";
-import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-<<<<<<< HEAD
-        <Navbar />
         <Routes>
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Home />} />
-=======
-        <Routes>
-          {
-            routes.map((route, index) => (
-              <Route 
-                path={route.path}
-                element={<route.component/>}
-              />
-            ))
-          }
->>>>>>> 42ad5919b9a7a5835324b682644904f0fc75c5af
+          {routes.map((route, index) => (
+            <Route path={route.path} element={<route.component />} />
+          ))}
         </Routes>
       </BrowserRouter>
     </div>
