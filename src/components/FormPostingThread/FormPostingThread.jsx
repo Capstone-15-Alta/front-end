4b9e33903698b7daef1dfdd2d2d7dc6bbe274525
muhamadjaya <ahmadzainsiji@gmail.com>
2022-06-09 22:29:49 +0700

    add style on form posting thread

--- src/components/FormPostingThread/FormPostingThread.jsx
+++ src/components/FormPostingThread/FormPostingThread.jsx
@@ -28,67 +28,89 @@ const FormPostingThread = () => {
     setInputs(newInputs);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
-      <div className="row form-posting-thread justify-content-center">
-        <div className="user-thread">
-          <div className="user-thread-icon">
-            <img
-              src="/assets/icon/man profil.png"
-              alt="user icon"
-              className="user-icon"
-            />
-          </div>
-          <div className="user-name-and-email">
-            <h5 className="user-name">Muhammad Yogi</h5>
-            <p className="user-email">Muhamadyogi413@gmail.com</p>
-          </div>
-          <div className="user-check-icon">
-            <img
-              src="/assets/icon/check.png"
-              alt="user icon"
-              className="user-check"
-            />
-          </div>
-          <div className="posting-time">
-            <p className="time-to-post">Hari ini 20:00</p>
-          </div>
-          <div className="options-thread-categories">
-            <select
-              name="kategori"
-              className="form-select shadow-none select-option-category"
-              aria-label="Default select kategori wisata"
-              defaultValue=""
-              onChange={(e) => handleInput(e.target.value, e.target.name)}
-            >
-              <option value="" hidden>
-                Pilih Kategori
-              </option>
-              {categories.map((dataCategory, dataCategoryIdx) => (
-                <option key={dataCategoryIdx} value={dataCategory}>
-                  {dataCategory}
+      <form
+        onSubmit={handleSubmit}
+        onReset={handleReset}
+        className="form-post-user-thread"
+      >
+        <div className="row form-posting-thread">
+          <div className="user-thread">
+            <div className="user-thread-icon">
+              <img
+                src="/assets/icon/man profil.png"
+                alt="user icon"
+                className="user-icon"
+              />
+            </div>
+            <div className="user-name-and-email">
+              <h5 className="user-name">Muhammad Yogi</h5>
+              <p className="user-email">Muhamadyogi413@gmail.com</p>
+            </div>
+            <div className="user-check-icon">
+              <img
+                src="/assets/icon/check.png"
+                alt="user icon"
+                className="user-check"
+              />
+            </div>
+            <div className="posting-time">
+              <p className="time-to-post">Hari ini 20:00</p>
+            </div>
+            <div className="options-thread-categories">
+              <select
+                name="kategori"
+                className="form-select shadow-none select-option-category"
+                aria-label="Default select kategori wisata"
+                defaultValue=""
+                onChange={(e) => handleInput(e.target.value, e.target.name)}
+              >
+                <option value="" hidden>
+                  Pilih Kategori
                 </option>
-              ))}
-            </select>
+                {categories.map((dataCategory, dataCategoryIdx) => (
+                  <option key={dataCategoryIdx} value={dataCategory}>
+                    {dataCategory}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
+          <input
+            type="text"
+            className="form-control shadow-none thread-title"
+            name="judul"
+            placeholder="Isi Judul Thread Disini"
+            value={inputs.judul}
+            onChange={(e) => handleInput(e.target.value, e.target.name)}
+          />
+          <textarea
+            className="form-control shadow-none thread-desc"
+            name="deskripsi"
+            rows="7"
+            placeholder="Apa Yang Ingin Anda Diskusikan ?"
+            value={inputs.deskripsi}
+            onChange={(e) => handleInput(e.target.value, e.target.name)}
+          />
         </div>
-        <input
-          type="text"
-          className="form-control shadow-none thread-title"
-          name="judul"
-          placeholder="Isi Judul Thread Disini"
-          value={inputs.judul}
-          onChange={(e) => handleInput(e.target.value, e.target.name)}
-        />
-        <textarea
-          className="form-control shadow-none thread-desc"
-          name="deskripsi"
-          rows="7"
-          placeholder="Apa Yang Ingin Anda Diskusikan ?"
-          value={inputs.deskripsi}
-          onChange={(e) => handleInput(e.target.value, e.target.name)}
-        />
-      </div>
+        <div className="button-area">
+          <button type="reset" className="btn btn-kembali">
+            Kembali
+          </button>
+          <button type="submit" className="btn btn-posting">
+            Posting
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/BuatThread/BuatThread.jsx
+++ src/pages/BuatThread/BuatThread.jsx
@@ -3,12 +3,14 @@ import "./BuatThread.scss";
 
 import Navigationbar from "../../components/Navigationbar/Navigationbar";
 import FormPostingThread from "../../components/FormPostingThread/FormPostingThread";
+import Footer from "../../components/footer/Footer.js";
 
 const BuatThread = () => {
   return (
     <div>
       <Navigationbar />
       <FormPostingThread />
+      <Footer />
     </div>
   );
 };
