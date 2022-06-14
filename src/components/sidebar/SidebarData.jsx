de64c83ac0070d36a31e83afc99c11393b839e47
za-king <muhammadzakysyukur@gmail.com>
2022-06-14 16:36:42 +0700

    Update SidebarData.jsx

--- src/components/sidebar/SidebarData.jsx
+++ src/components/sidebar/SidebarData.jsx
@@ -1,17 +1,17 @@
-import logo1 from "./icon-sidebar/Vector home.png";
-import logo2 from "./icon-sidebar/Vector notifikasi.png";
-import logo3 from "./icon-sidebar/Vector explore.png";
-import logo4 from "./icon-sidebar/Vector profil.png";
+import logo1 from "../../images/icon-sidebar/Vector home.png";
+import logo2 from "../../images/icon-sidebar/Vector notifikasi.png";
+import logo3 from "../../images/icon-sidebar/Vector explore.png";
+import logo4 from "../../images/icon-sidebar/Vector profil.png";
 
-import man1 from "./icon-sidebar/man1.png";
-import man2 from "./icon-sidebar/man2.png";
-import man3 from "./icon-sidebar/man3.png";
-import man4 from "./icon-sidebar/man4.png";
+import man1 from "../../images/icon-sidebar/man1.png";
+import man2 from "../../images/icon-sidebar/man2.png";
+import man3 from "../../images/icon-sidebar/man3.png";
+import man4 from "../../images/icon-sidebar/man4.png";
 
-import hover1 from "./icon-sidebar/Vector (4).png";
-import hover2 from "./icon-sidebar/Vector (5).png";
-import hover3 from "./icon-sidebar/Vector (6).png";
-import hover4 from "./icon-sidebar/Vector (7).png";
+import hover1 from "../../images/icon-sidebar/Vector (4).png";
+import hover2 from "../../images/icon-sidebar/Vector (5).png";
+import hover3 from "../../images/icon-sidebar/Vector (6).png";
+import hover4 from "../../images/icon-sidebar/Vector (7).png";
 export const data = {
   left: [
     {
@@ -24,13 +24,13 @@ export const data = {
       title: "Notifikasi",
       icon: <img src={logo2} alt="" width={20} />,
       icon2: <img src={hover3} alt="" width={20} />,
-      link: "buat-thread",
+      link: "/buat-thread",
     },
     {
       title: "Explore Topik",
       icon: <img src={logo3} alt="" width={20} />,
       icon2: <img src={hover4} alt="" width={20} />,
-      link: "explore-topik",
+      link: "/explore-topik",
     },
     {
       title: "Profile",
@@ -44,22 +44,26 @@ export const data = {
     {
       name: "Charlie005",
       icon: <img src={man1} alt="" width={40} />,
-      link: "link",
+      follow: true,
+      link: "/link",
     },
     {
       name: "AlexBrown",
       icon: <img src={man2} alt="" width={40} />,
-      link: "link",
+      follow: false,
+      link: "/link",
     },
     {
       name: "Emma_Wright",
       icon: <img src={man3} alt="" width={40} />,
-      link: "link",
+      follow: false,
+      link: "/link",
     },
     {
       name: "Ana2007",
       icon: <img src={man4} alt="" width={40} />,
-      link: "link",
+      follow: true,
+      link: "/link",
     },
   ],
 };
