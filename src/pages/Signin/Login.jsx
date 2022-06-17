8d71871b93a5ff173fd30cf04ef9077f3a7c9d59
za-king <muhammadzakysyukur@gmail.com>
2022-06-17 11:55:47 +0700

    form valiadtion

--- src/pages/Signin/Login.jsx
+++ src/pages/Signin/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import Navbar from "../../components/Navbar";
 import Container from "react-bootstrap/Container";
 import Grid from "@mui/material/Grid";
@@ -9,6 +9,56 @@ import Button from "react-bootstrap/Button";
 import Footer from "../../components/footer/Footer";
 
 export default function Login() {
+  const intialValues ={
+    username: "",
+    password:""
+  }
+
+  const [formValues,setFormValues] =useState(intialValues)
+  const [formError,setFormError] =useState({})
+  const [isSubmit , setIsSubmit] =useState(false)
+
+  const handleformValues =(e)=>{
+    const name = e.target.name
+    setFormValues({...formValues , [name] :e.target.value})
+    
+  }
+
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+    setFormError(validate(formValues))
+    setIsSubmit(true)
+  }
+
+  useEffect(() =>{
+    console.log(formError)
+    if(Object.keys(formError).length === 0 && isSubmit ){
+      console.log(formValues)
+    }
+  },[formError])
+
+  
+
+  const validate =(value)=>{
+    const error = {}
+    const regex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!value.username){
+      error.username = "Username is required"
+    }else if(!regex.test(value.username)){
+      error.username = "not valid email"
+    }
+    if(!value.password){
+      error.password = "Password is required"
+    }else if(value.password < 4){
+      error.password = "Password must be more than 4 character"
+    }else if(value.password > 10){
+      error.password = "Password cannot exceed more than 10 character"
+    }
+
+    return error
+
+  }
   return (
     <>
       <Navbar />
@@ -41,17 +91,20 @@ export default function Login() {
                 Halo lagi, Anda telah dirindukan!
               </Typography>
               <Box pt="1vw">
-                <Form>
+              <Form onSubmit={handleSubmit}>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label style={{ color: "#26B893" }}>
-                      Nomor Handphone
+                      Email
                     </Form.Label>
                     <Form.Control
-                      type="number"
-                      placeholder="+62 | Masukan Nomor Handphone"
+                      type="input"
+                      placeholder=" UserName"
+                      name="username"
+                      value={formValues.username}
+                      onChange={handleformValues}
                     />
                   </Form.Group>
-
+                  <div>{formError.username}</div>
                   <Form.Group className="mb-2" controlId="formBasicPassword">
                     <Form.Label style={{ color: "#26B893" }}>
                       Kata Sandi
@@ -59,8 +112,12 @@ export default function Login() {
                     <Form.Control
                       type="password"
                       placeholder="Enter your password"
+                      name="password"
+                      value={formValues.password}
+                      onChange={handleformValues}
                     />
                   </Form.Group>
+                  <div>{formError.password}</div>
                   <a
                     href="/lupa-password"
                     style={{ textDecoration: "none", color: "#26B893" }}
