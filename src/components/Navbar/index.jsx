99a5b82d8dbf43f7b08d822c9c7903b7c64ec5e9
DargonFly378 <hafichever@gmail.com>
2022-06-19 15:32:10 +0700

    responsive di navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import "./Navbar.scss";
 
 import { useNavigate } from "react-router-dom";
 import { Link, NavLink } from "react-router-dom";
-import NavLogo from "../../assets/images/navlogo.png";
+import logo from "../../assets/images/logoNavbar.svg";
 
 import IconProfile from "../IconProfile";
 
@@ -17,8 +17,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light fixed-top customNav shadow-sm">
       <div className="container-fluid">
         <Link className="navbar-brand navBrand" to="/">
-          <img src={NavLogo} alt="logo" className="navLogo" />
-          <span className="textLogo">Forum Group Diskusi</span>
+          <img src={logo} alt="logo" className="navLogo" />
         </Link>
         <button
           className="navbar-toggler"
@@ -31,16 +30,22 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <Searchbar />
+        <div className="collapse navbar-collapse text-center" id="navbarNav">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Searchbar />
+            </li>
+            <li class="nav-item">
+              <Button
+                title="Buat Thread"
+                background="white"
+                type="button"
+                className="btn-create-new-thread"
+                iconKiri="iconCreate"
+              />
+            </li>
+          </ul>
 
-          <Button
-            title="Buat Thread"
-            background="white"
-            type="button"
-            className="btn-create-new-thread"
-            iconKiri="iconCreate"
-          />
           <Link className="navbar-user-icon ms-auto" to="/">
             {/* <img src={IconProfile} alt="user icon" className="user-icon" /> */}
             <IconProfile />
