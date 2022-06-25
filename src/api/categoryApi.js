3aba891ec22c25ba2a6eb0dd191eaac24a5d1523
za-king <muhammadzakysyukur@gmail.com>
2022-06-26 00:18:59 +0700

    category udah fix nih agan agan hehehe

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -4,7 +4,12 @@ const categoryApi = {
   getCategory: (params) => {
     const url = "category";
     return axiosClient.get(url, params);
-  }
+  },
+  getThread: (value) => {
+    const url = "thread/";
+    console.log(value)
+    return axiosClient.get(url + `?category=` + `${value}`);
+  },
 };
 
 export default categoryApi;
