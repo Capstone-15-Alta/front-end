cc5f4978c9c8a7079b02c24382b14fe0f1603d69
syukronarie <syukronarie@gmail.com>
2022-07-13 21:05:58 +0700

    fix image

--- src/components/HeaderProfile/index.jsx
+++ src/components/HeaderProfile/index.jsx
@@ -1,16 +1,17 @@
-import React, { useRef } from "react";
-import Button from "../Button/Button";
+import React, { useRef } from 'react';
+import Button from '../Button/Button';
 
-import "./HeaderProfile.scss";
+import './HeaderProfile.scss';
 
 /* Images */
 // import banner from "../../assets/images/capung.png";
 // import foto from "../../assets/images/foto.jpg";
-import { Link, useLocation, useParams } from "react-router-dom";
-import fgdApi from "../../api/fgdApi";
-import Cookies from "js-cookie";
-import { Avatar } from "@mui/material";
-import Swal from "sweetalert2";
+import { Link, useLocation, useParams } from 'react-router-dom';
+import fgdApi from '../../api/fgdApi';
+import Cookies from 'js-cookie';
+import { Avatar } from '@mui/material';
+import Swal from 'sweetalert2';
+import axiosClient from '../../api/axiosClient';
 
 const HeaderProfile = ({ data, getUserById }) => {
   const { id } = useParams();
@@ -19,13 +20,34 @@ const HeaderProfile = ({ data, getUserById }) => {
   const bannerRef = useRef(null);
   const imageRef = useRef(null);
 
-  const tokenCookies = Cookies.get("token");
-  const userId = Cookies.get("id");
+  const tokenCookies = Cookies.get('token');
+  const userId = Cookies.get('id');
   // console.log(tokenCookies, userId);
 
   // const [bannerImg, setBannerImg] = useState(banner);
 
   // const [photoImg, setPhotoImg] = useState();
+
+  // const [imageData, setImageData] = React.useState({loaded: false, data});
+  // const fetchImage = React.useCallback(async () => {
+  //   const url = data.image.slice(19);
+  //   const result = await axiosClient.get(url);
+  //   setImageData({loaded: true, data: result});
+  // }, [data.image]);
+
+  // React.useEffect(() => {
+  //   if (data.image) fetchImage();
+  // }, [data.image, fetchImage]);
+
+  const [imageUrl, setImageUrl] = React.useState('');
+
+  React.useEffect(() => {
+    if (data.image) {
+      const url = data.image.slice(19);
+
+      setImageUrl(url);
+    }
+  }, [data.image]);
 
   const handleClickImage = () => {
     imageRef.current.click();
@@ -41,24 +63,24 @@ const HeaderProfile = ({ data, getUserById }) => {
 
     try {
       const formData = new FormData();
-      formData.set("file", fileBanner);
+      formData.set('file', fileBanner);
 
       await fgdApi.uploadBanner(tokenCookies, formData);
       // console.log(res);
       getUserById(userId);
 
       Swal.fire({
-        title: "Success",
-        text: "Banner berhasil diganti!",
-        icon: "success",
-        confirmButtonText: "OK",
+        title: 'Success',
+        text: 'Banner berhasil diganti!',
+        icon: 'success',
+        confirmButtonText: 'OK',
       });
     } catch (error) {
       Swal.fire({
-        title: "Failed",
-        text: "Terjadi Kesalahan, silahkan upload gambar dengan ukuran lebih kecil",
-        icon: "error",
-        confirmButtonText: "OK",
+        title: 'Failed',
+        text: 'Terjadi Kesalahan, silahkan upload gambar dengan ukuran lebih kecil',
+        icon: 'error',
+        confirmButtonText: 'OK',
       });
     }
   };
@@ -69,23 +91,23 @@ const HeaderProfile = ({ data, getUserById }) => {
 
     try {
       const formData = new FormData();
-      formData.set("file", filePhoto);
+      formData.set('file', filePhoto);
 
       await fgdApi.uploadPhoto(tokenCookies, formData);
       // console.log(res);
       getUserById(userId);
       Swal.fire({
-        title: "Success",
-        text: "Foto berhasil diganti!",
-        icon: "success",
-        confirmButtonText: "OK",
+        title: 'Success',
+        text: 'Foto berhasil diganti!',
+        icon: 'success',
+        confirmButtonText: 'OK',
       });
     } catch (error) {
       Swal.fire({
-        title: "Failed",
-        text: "Terjadi Kesalahan, silahkan upload gambar dengan ukuran lebih kecil",
-        icon: "error",
-        confirmButtonText: "OK",
+        title: 'Failed',
+        text: 'Terjadi Kesalahan, silahkan upload gambar dengan ukuran lebih kecil',
+        icon: 'error',
+        confirmButtonText: 'OK',
       });
     }
 
@@ -103,94 +125,78 @@ const HeaderProfile = ({ data, getUserById }) => {
 
   return (
     <>
-      <div className="headersss">
-        <div className="banner">
-          <div
-            style={{ backgroundImage: `url(${data.image_cover})` }}
-            className="banner-image"
-          ></div>
-          {path === "/profile" || "edit-profile" ? (
+      <div className='headersss'>
+        <div className='banner'>
+          <div style={{ backgroundImage: `url(${data.image_cover})` }} className='banner-image'></div>
+          {path === '/profile' || 'edit-profile' ? (
             <>
               <Button
-                type="button"
-                className=" btn btnChangeBanner "
-                title="Ganti Gambar"
-                background="#26b893"
-                color="#fff"
-                iconKiri="iconKamera"
+                type='button'
+                className=' btn btnChangeBanner '
+                title='Ganti Gambar'
+                background='#26b893'
+                color='#fff'
+                iconKiri='iconKamera'
                 onClick={handleClickBanner}
               />
-              <input
-                type="file"
-                className="d-none"
-                ref={bannerRef}
-                onChange={handleUploadBanner}
-              />
+              <input type='file' className='d-none' ref={bannerRef} onChange={handleUploadBanner} />
             </>
           ) : (
-            ""
+            ''
           )}
         </div>
 
-        <div className="profile-zzz">
-          <Avatar className="foto" alt={data.username} src={data.image} />
-
-          {path === "/profile" || "edit-profile" ? (
+        <div className='profile-zzz'>
+          {!!imageUrl ? (
+            <Avatar className='foto' alt={data.username} src={axiosClient.get(imageUrl)} />
+          ) : (
+            <p>loading</p>
+          )}
+          {path === '/profile' || 'edit-profile' ? (
             <>
               <Button
-                type="button"
-                className=" btn btnChangeImage "
-                background="#26b893"
-                color="#fff"
-                iconKiri="iconKamera"
+                type='button'
+                className=' btn btnChangeImage '
+                background='#26b893'
+                color='#fff'
+                iconKiri='iconKamera'
                 onClick={handleClickImage}
               />
-              <input
-                type="file"
-                className="d-none"
-                ref={imageRef}
-                onChange={handleUploadImage}
-              />
+              <input type='file' className='d-none' ref={imageRef} onChange={handleUploadImage} />
             </>
           ) : (
-            ""
+            ''
           )}
-          <div className="dataProfile">
-            <div className="nickname d-flex ">
-              <div className="name-email">
-                <p className="name">{data.username}</p>
-                <p className="mail">{data.email}</p>
+          <div className='dataProfile'>
+            <div className='nickname d-flex '>
+              <div className='name-email'>
+                <p className='name'>{data.username}</p>
+                <p className='mail'>{data.email}</p>
               </div>
-              {path === "/profile" ? (
-                <div className="editBtn">
-                  <Link to="/edit-profile">
-                    <Button
-                      type="button"
-                      className="btn btnEditProfile"
-                      title="Edit Profile"
-                    />
+              {path === '/profile' ? (
+                <div className='editBtn'>
+                  <Link to='/edit-profile'>
+                    <Button type='button' className='btn btnEditProfile' title='Edit Profile' />
                   </Link>
                 </div>
-              ) : path === "/edit-profile" ? (
-                ""
+              ) : path === '/edit-profile' ? (
+                ''
               ) : (
-                <div className="editBtn">
-                  {data.user_followers?.filter(
-                    (is_follow) => is_follow.user_follower.id == userId
-                  ).length > 0 ? (
+                <div className='editBtn'>
+                  {data.user_followers?.filter((is_follow) => is_follow.user_follower.id == userId).length > 0 ? (
                     <Button
-                      type="button"
-                      className="btn btnIkuti"
-                      title="✓ mengikuti"
+                      type='button'
+                      className='btn btnIkuti'
+                      title='✓ mengikuti'
                       onClick={(e) => {
                         followHandleClick(e, id);
                       }}
                     />
                   ) : (
                     <Button
-                      type="button"
-                      className="btn btnIkuti"
-                      title="+ Ikuti"
+                      type='button'
+                      className='btn btnIkuti'
+                      title='+ Ikuti'
                       onClick={(e) => {
                         followHandleClick(e, id);
                       }}
